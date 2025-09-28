64a5ae98bf8f44f6d4db2d6955fe263beb053e59
Marlon Silva <msilva20162016@hotmail.com>
2025-09-28 17:14:17 -0300

    Add ListTransactionsUseCase for retrieving all transactions

---
+++ src/application/use-cases/ListTransactionsUseCase.ts
@@ -0,0 +1,10 @@
+import { Transaction } from '@domain/entities/Transaction';
+import { ITransactionRepository } from '@domain/repositories/ITransactionRepository';
+
+export class ListTransactionsUseCase {
+    constructor(private transactionRepository: ITransactionRepository) { }
+
+    async execute(): Promise<Transaction[]> {
+        return await this.transactionRepository.findAll();
+    }
+}
