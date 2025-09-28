a2d2cb029fef5336fdf591fa0fb3469a53e8a59f
Marlon Silva <msilva20162016@hotmail.com>
2025-09-28 16:08:56 -0300

    Add Money value object with methods for amount validation

---
+++ src/domain/value-objects/Money.ts
@@ -0,0 +1,27 @@
+export class Money {
+  constructor(
+    private readonly amount: number,
+  ) {
+    if (amount < 0) {
+      throw new Error('Money amount cannot be negative');
+    }
+  }
+
+  getValue(): number {
+    return this.amount;
+  }
+
+  format(): string {
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+    }).format(this.amount);
+  }
+
+  add(other: Money): Money {
+    return new Money(this.amount + other.amount);
+  }
+
+  subtract(other: Money): Money {
+    return new Money(this.amount - other.amount);
+  }
+}
